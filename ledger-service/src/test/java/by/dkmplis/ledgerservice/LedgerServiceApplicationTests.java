05958da2cc80046f43340ba0e21a770899f6aa26
dkmplis <dkmplis@mail.ru>
2026-08-21 15:04:38 +0300

    Initial commit

---
+++ ledger-service/src/test/java/by/dkmplis/ledgerservice/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dkmplis.ledgerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
